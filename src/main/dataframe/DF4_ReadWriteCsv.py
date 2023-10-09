92f655763bd144d95f9175ed0563eedecfbed901
nachiket-personal <nachiketsbhosale@gmail.com>
2023-10-09 22:27:44 +0530

    read write csv file in dataframe

--- src/main/dataframe/DF4_ReadWriteCsv.py
+++ src/main/dataframe/DF4_ReadWriteCsv.py
@@ -1,3 +1,4 @@
+from pyspark.sql.functions import col
 from pyspark.sql.types import StructType, StringType, IntegerType, FloatType
 
 from src.main.utilities.SparkSessionHandler import create_spark_session
@@ -86,7 +87,7 @@ emp_schema = (StructType()
 
 
 emp_df5 = spark.read.option("header", True).schema(schema=emp_schema).csv(emp_input_path2)
-emp_df5.show(3)
+#emp_df5.show(3)
 '''
 +-----------+--------+---+----------+--------+------+
 |Employee_Id|    Name|Age|Department|  Salary|Gender|
@@ -96,7 +97,7 @@ emp_df5.show(3)
 |          3|Angelina| 25|        IT| 93000.3|   'F'|
 +-----------+--------+---+----------+--------+------+
 '''
-emp_df5.printSchema()
+#emp_df5.printSchema()
 '''
 root
  |-- Employee_Id: string (nullable = true)
@@ -107,4 +108,72 @@ root
  |-- Gender: string (nullable = true)
  '''
 
+#emp_df6 = emp_df5.select("Employee_Id", "Department", "Salary")
+#emp_df6.show(2, False)
+'''
++-----------+----------+--------+
+|Employee_Id|Department|Salary  |
++-----------+----------+--------+
+|1          |IT        |80000.5 |
+|2          |IT        |78000.25|
++-----------+----------+--------+
+'''
+#emp_df6.printSchema()
+'''
+root
+ |-- Employee_Id: string (nullable = true)
+ |-- Department: string (nullable = true)
+ |-- Salary: float (nullable = true)
+ '''
+
+emp_df7 = emp_df5.select(col("Name").alias("name"), col("Age"), col("Gender"))
+emp_df7.show(2, False)
+'''
++------+---+------+
+|Name  |Age|Gender|
++------+---+------+
+|Daniel|32 |'M'   |
+|Alex  |33 |'M'   |
++------+---+------+
+'''
+
+#emp_df5.select(emp_df5["Employee_Id"].alias("EmpId"), emp_df5["Salary"].alias("Sal")).show(2, False)
+'''
++-----+--------+
+|EmpId|Sal     |
++-----+--------+
+|1    |80000.5 |
+|2    |78000.25|
++-----+--------+
+'''
+
+#emp_df5.select(emp_df5["Employee_Id"].alias("EmpId"), col("Salary").alias("Sal")).show(2, False)
+'''
++-----+--------+
+|EmpId|Sal     |
++-----+--------+
+|1    |80000.5 |
+|2    |78000.25|
++-----+--------+
+'''
+
+#emp_df5.select(emp_df5.Employee_Id).show(2, False)
+'''
++-----------+
+|Employee_Id|
++-----------+
+|1          |
+|2          |
++-----------+
+'''
+
+emp_csv_output = "..\\resources\\output\\emp_csv_df_output"
+emp_df7.write.option("header", True).mode("overwrite").format("csv").save(emp_csv_output)
+
+#emp_df7.write.option("header", True).mode("append").format("csv").save(emp_csv_output)
+#emp_df7.write.option("header", True).mode("ignore").format("csv").save(emp_csv_output)
+#emp_df7.write.option("header", True).mode("error").format("csv").save(emp_csv_output)
+
+emp_json_output = "..\\resources\\output\\emp_json_df_output"
+emp_df7.write.mode("overwrite").format("json").save(emp_json_output)
 spark.stop()
